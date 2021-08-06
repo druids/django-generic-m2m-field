b0eafeef9cb80ad80867a4e9c4cff5b86a61a3a9
Luboš Mátl <matllubos@gmail.com>
2021-08-06 11:30:40 +0200

    0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-generic-m2m-field',
-    version='0.0.5',
+    version='0.0.6',
     description="Django generic many to many field library.",
     keywords='django, fields, generic',
     author='Lubos Matl',
